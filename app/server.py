3ab2dc534b8c5a083d49940cf9ece9d5cd1dd209
yoshinori77 <yoshinoriyamashita123@gmail.com>
2022-01-23 15:52:22 +0900

    Add urls

--- app/server.py
+++ app/server.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
 
-from .dashapp import create_dash_app
+from app.dash2 import create_dash2_app
+
+from app.dashapp import create_dash_app
+from app.simple_dashboard import create_simple_dashboard_app
 
 
 app = FastAPI()
-
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
 
 
 @app.get("/")
@@ -33,3 +31,18 @@ def get_status():
 # it to root
 dash_app = create_dash_app(requests_pathname_prefix="/dash/")
 app.mount("/dash", WSGIMiddleware(dash_app.server))
+
+simple_dash_app = create_simple_dashboard_app(requests_pathname_prefix="/simple_dash/")
+app.mount("/simple_dash", WSGIMiddleware(simple_dash_app.server))
+
+
+dash2_app = create_dash2_app(requests_pathname_prefix="/dash2/")
+app.mount("/dash2", WSGIMiddleware(dash2_app.server))
+
+# todo ファンダメンタル分析とテクニカル分析のURLを別に用意
+# tech_app = create_tech_app(requests_pathname_prefix="/tech/")
+# app.mount("/tech", WSGIMiddleware(tech_app.server))
+
+# fund_app = create_fund_app(requests_pathname_prefix="/fund/")
+# app.mount("/fund", WSGIMiddleware(fund_app.server))
+
